4dfc0508483d31f2f934bda47de08aa4e8838cb2
Chandan Kumar Saw <chandan1951c@gmail.com>
2025-02-04 04:31:11 +0530

    backup dept and home till 4 feb 2025

---
+++ src/components/backup/dept.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import departmentsData from '../data/departments.json';
+import Button from './Button';
+import Uploadorganiser from './alert/Uploadorganiser';
+import Price from './Price';
+
+const Department = () => {
+  const { deptName } = useParams();
+  const department = departmentsData[deptName];
+
+  if (!department) {
+    return <h2 className="text-xl font-bold text-red-600">Department not found</h2>;
+  }
+
+  return (
+    <div className="bg-red-300 min-h-screen">
+      <div className="text-center pt-16">
+        <h1 className="text-3xl font-bold mb-6">{deptName} Department</h1>
+        {/* alert */}
+
+        <Uploadorganiser />
+
+
+        <div className="flex flex-wrap justify-center gap-6">
+          {Object.keys(department.semesters).map((sem) => {
+            const semesterLink = department.semesters[sem];
+
+            return (
+              <div
+                className="bg-gray-100 border border-gray-300 rounded-lg p-4 w-64 shadow-md hover:shadow-lg transition-shadow duration-200"
+                key={sem}
+              >
+                <img
+                  // src={`../public/assets/${deptName}/${deptName}${sem}.jpg`}
+                  src={`/assets/${deptName}/${deptName}.jpg`}
+                  alt={`${deptName} Semester ${sem} Organizer`}
+                  className="mb-4 rounded-lg"
+                />
+                <h2 className="text-xl font-semibold mb-2">Semester {sem}</h2>
+
+
+
+                {/* Check if semester link is valid before rendering the button */}
+                {semesterLink && semesterLink.length > 0 ? (
+                  <>
+
+                    {/* adding price component */}
+
+                    <Price discountedPrice={1} originalPrice={100} discountPercentage={99} />
+
+                    <a href={semesterLink} target="_blank" rel="noopener noreferrer">
+                      {/* <Button name={`Pay For Sem ${sem}`} /> */}
+                      <Button name={`Buy Now`} />
+                    </a>
+                  </>
+                ) : (
+                  <span className="text-gray-500 mb-2">Organizer Not Available</span>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default Department;
